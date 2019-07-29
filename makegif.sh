79bef72c0126b8ba275e4a65077e925b1279ed71
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2019-07-29 14:12:21 -0400

    longer delays in the gif

--- makegif.sh
+++ makegif.sh
@@ -1,2 +1,4 @@
+# NOTE: this overwrite the hires images with low res so be sure 
+# to make a copy first
 mogrify -resize 1920x1080 *png
-convert -delay 50 *.png -delay 300 img0012.png tess_south.gif
+convert -delay 70 *.png -delay 350 img0012.png tess_south.gif
