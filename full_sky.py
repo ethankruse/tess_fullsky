cef72dca847eef4583319e1e083f50b5682a623d
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2019-12-01 21:21:58 -0500

    adding corner adjustments for S14-16

--- full_sky.py
+++ full_sky.py
@@ -21,7 +21,7 @@ figdir = os.path.join(os.path.split(__file__)[0], 'figs')
 cornerdir = os.path.join(os.path.split(__file__)[0], 'corners')
 
 # options are 'north', 'south', or 'both'
-hemisphere = 'both'
+hemisphere = 'north'
 
 # parameters that change depending on the hemisphere
 if hemisphere == 'both':
@@ -75,26 +75,26 @@ cmap = truncate_colormap(plt.get_cmap(cmap), minval=0.18, maxval=1.0)
 
 # do we need to create the empirical corner glow correction for a sector?
 makecorner = False
-cornersec = 14
+cornersec = 16
 
 # remove the corner glow from the final image
 remove_corner_glow = True
 # make a plot of the corner glow for every CCD to check how removal is working
-corner_glow_plot = True
+corner_glow_plot = False
 
 # manual adjustments to the strength of corner glow corrections
 adjfile = os.path.join(cornerdir, 'adjustments.txt')
 
 # flag indicating we're just testing things
-test = True
+test = False
 # create the output figure
-makefig = False
+makefig = True
 # the output figure in full resolution
-highres = False
+highres = True
 # save the output figure
-savefig = False
+savefig = True
 # save every sector image for a gif in a subdirectory
-makegif = False
+makegif = True
 if makegif:
     figdir = os.path.join(figdir, f'gif_{fbase}')
 # use a transparent background instead of white
@@ -174,7 +174,7 @@ if makegif:
 # anything we want to test
 if test:
     # files = files[187:188]
-    files = glob(os.path.join(datadir, f'*s00{cornersec:02d}-1*fits'))
+    files = glob(os.path.join(datadir, f'*s0014-4-4*fits'))
     files.sort()
     
 
@@ -307,6 +307,9 @@ for ii, ifile in enumerate(files):
                 data = clean_corner(data, cornerdir, adjustments,
                                     cleanplot=corner_glow_plot, ccd=iccd,
                                     sec=isec, cam=icam)
+                if corner_glow_plot:
+                    plt.figure()
+                    plt.imshow(data, norm=cnorm, cmap=cmap)
 
         # some special processing to avoid problem areas.
         # these are all in overlap zones, so removing contaminated chunks
