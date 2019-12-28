36c2e1d18c96fa9bfd84075e2d47ce445ec25dca
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2019-12-28 16:24:00 -0500

    revert to south

--- makegif.sh
+++ makegif.sh
@@ -1,4 +1,4 @@
 # NOTE: this overwrite the hires images with low res so be sure 
 # to make a copy first
 mogrify -resize 1920x1080 *png
-convert -dispose previous -delay 70 *.png -delay 350 img0016.png tess_sky.gif
+convert -dispose previous -delay 70 *.png -delay 350 img0013.png tess_south.gif
